9a38d5c74f2f38709d51530fce2a7836e5fe0e52
aidan23smith <aidan23smith@gmail.com>
2023-06-06 15:05:47 +0100

    AS: add base page

---
+++ app/components/page/styles.ts
@@ -0,0 +1,7 @@
+import { SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+
+export const Page = styled(SafeAreaView)`
+  width: 80%;
+  min-width: 100px;
+`
--- app/views/jounals/index.tsx
+++ app/views/jounals/index.tsx
@@ -2,27 +2,28 @@ import {
     Text,
   View,
 } from "react-native";
-import Page from "../../components/page/Page";
 import {GovukH1} from "../../components/h1/styles";
 import {GovukH2} from "../../components/h2/styles";
 import {GovukInset} from "../../components/inset/styles";
 import {GovukHeader} from "../../components/header/styles";
+import {Page} from "../../components/page/styles";
 
 const JournalPage = () => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>otherpage</Text>
-            <Page/>
-            <GovukH1>GovukH1</GovukH1>
-            <GovukH2>GovukH2</GovukH2>
-            <GovukHeader>
-                <Text>Header</Text>
-            </GovukHeader>
-            <GovukInset>
-                <Text>
-                    Inset
-                </Text>
-            </GovukInset>
+            <Page>
+                <Text>otherpage</Text>
+                <GovukH1>GovukH1</GovukH1>
+                <GovukH2>GovukH2</GovukH2>
+                <GovukHeader>
+                    <Text>Header</Text>
+                </GovukHeader>
+                <GovukInset>
+                    <Text>
+                        Inset
+                    </Text>
+                </GovukInset>
+            </Page>
         </View>
 
     )
