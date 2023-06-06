beabbe1159225ca9b7e5867f27a8e20efee27461
thomaspowell <thomas.powell@engineering.digital.dwp.gov.uk>
2023-06-06 16:25:57 +0100

    update components

---
+++ app/components/text/styles.ts
@@ -0,0 +1,17 @@
+import {Text, View} from "react-native";
+import styled from "styled-components/native";
+
+export const GovukPanel = styled(View)`
+max-width: 38em;
+margin-bottom: 20px;
+font-size: 19px;
+line-height: 1.3157894737;
+color: #0b0c0c;
+font-weight: 400;
+margin-top: 0;
+display: block;
+margin-block-start: 1em;
+margin-block-end: 1em;
+background-color: #f3f2f1;
+text-size-adjust: 100%;
+`
