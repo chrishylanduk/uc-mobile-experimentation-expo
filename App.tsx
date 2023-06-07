b6a78b97989687e27c0c4050c5609e7c79c3163b
aidan23smith <aidan23smith@gmail.com>
2023-06-07 11:18:19 +0100

    AS: Make login process better

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Dispatch, SetStateAction, useEffect} from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import LoginPage from "./app/views/login";
 import ClaimantHomePage from "./app/views/claimant_home";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import SettingsPage from "./app/views/settings";
 import TodoPage from "./app/views/todos";
 import JournalPage from "./app/views/jounals";
-import {Dispatch, SetStateAction, useEffect} from "react";
 import * as Notifications from "expo-notifications";
 import * as Device from "expo-device";
-import { Subscription } from 'expo-modules-core';
-import {
-    Platform,
-} from "react-native";
+import {Subscription} from 'expo-modules-core';
+import {Platform,} from "react-native";
+import SplashPage from "./app/views/splash";
+import * as LocalAuthentication from "expo-local-authentication";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -65,6 +65,11 @@ export type RootStackParamList = {
   SignIn: undefined;
 };
 
+export type SignedOutParamList = {
+    Splash: undefined;
+    LoginButtonPage: undefined;
+};
+
 export type SignedInParamList = {
     Home: undefined;
     Todo: undefined;
@@ -80,6 +85,7 @@ export type UserIdContextType = {
 
 const SignedInStack = createBottomTabNavigator<SignedInParamList>();
 const TopLevel = createNativeStackNavigator<RootStackParamList>();
+const SignedOutStack = createNativeStackNavigator<SignedOutParamList>();
 
 function SignedInSection() {
   return (
@@ -129,6 +135,43 @@ function SignedInSection() {
   );
 }
 
+
+function SignedOutSection() {
+    const [facialRecognitionAvailable, setFacialRecognitionAvailable] = React.useState(false);
+    const [fingerprintAvailable, setFingerprintAvailable] = React.useState(false);
+    const [irisAvailable, setIrisAvailable] = React.useState(false);
+    const [checkedBio, setCheckedBio] = React.useState<Promise<boolean>>();
+
+    const checkSupportedAuthentication = async () => {
+        const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+        if (types && types.length) {
+            setFacialRecognitionAvailable(types.includes(LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION));
+            setFingerprintAvailable(types.includes(LocalAuthentication.AuthenticationType.FINGERPRINT));
+            setIrisAvailable(types.includes(LocalAuthentication.AuthenticationType.IRIS));
+        }
+        return true;
+    };
+
+    React.useEffect(() => {
+        setCheckedBio(checkSupportedAuthentication());
+    }, []);
+
+    return (
+        <SignedOutStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            {
+                    (facialRecognitionAvailable || fingerprintAvailable || irisAvailable || checkedBio) ?
+                    <SignedOutStack.Screen name="Splash" component={SplashPage} />//Todo pass in result to use in if above
+
+                    :
+                    <SignedOutStack.Screen name="LoginButtonPage" component={LoginPage} />
+            }
+        </SignedOutStack.Navigator>
+    );
+}
 
 export const PushNotificationTokenContext = React.createContext<String>("default");
 export const UserIdContext = React.createContext<UserIdContextType>({
@@ -189,7 +232,7 @@ function App() {
       >
           {
               userId === "" ?
-                  <TopLevel.Screen name="SignedOut" component={LoginPage} />
+                  <TopLevel.Screen name="SignedOut" component={SignedOutSection} />
                   :
                   <TopLevel.Screen name="SignIn" component={SignedInSection} />
           }
--- app/components/govuk/signin_button/index.tsx
+++ app/components/govuk/signin_button/index.tsx
@@ -16,24 +16,12 @@ enum EResult {
 type homeScreenProp = NativeStackNavigationProp<RootStackParamList, 'SignedOut'>;
 
 const SigninButton: React.FC = () => {
-  const [facialRecognitionAvailable, setFacialRecognitionAvailable] = React.useState(false);
-  const [fingerprintAvailable, setFingerprintAvailable] = React.useState(false);
-  const [irisAvailable, setIrisAvailable] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [result, setResult] = React.useState<EResult>();
   const {setUserId } = useContext(UserIdContext);
 
   const login = () => {
     setUserId("user id")
-  };
-
-  const checkSupportedAuthentication = async () => {
-    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
-    if (types && types.length) {
-      setFacialRecognitionAvailable(types.includes(LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION));
-      setFingerprintAvailable(types.includes(LocalAuthentication.AuthenticationType.FINGERPRINT));
-      setIrisAvailable(types.includes(LocalAuthentication.AuthenticationType.IRIS));
-    }
   };
 
   const authenticate = async () => {
@@ -44,7 +32,7 @@ const SigninButton: React.FC = () => {
     setLoading(true);
 
     try {
-      const results = await LocalAuthentication.authenticateAsync({promptMessage: description});
+      const results = await LocalAuthentication.authenticateAsync();
 
       if (results.success) {
         setUserId("user id")
@@ -66,63 +54,15 @@ const SigninButton: React.FC = () => {
   };
 
   React.useEffect(() => {
-    checkSupportedAuthentication();
+    authenticate();
   }, []);
-
-  let resultMessage;
-  switch (result) {
-    case EResult.CANCELLED:
-      resultMessage = 'Authentication process has been cancelled';
-      break;
-    case EResult.DISABLED:
-      resultMessage = 'Biometric authentication has been disabled';
-      break;
-    case EResult.ERROR:
-      resultMessage = 'There was an error in authentication';
-      break;
-    case EResult.SUCCESS:
-      resultMessage = 'Successfully authenticated';
-      break;
-    default:
-      resultMessage = '';
-      break;
-  }
-
-  let description : string;
-  if (facialRecognitionAvailable && fingerprintAvailable && irisAvailable) {
-    description = 'Log in with face, fingerprint or iris';
-  } else if (facialRecognitionAvailable && fingerprintAvailable) {
-    description = 'Log in with face or fingerprint';
-  } else if (facialRecognitionAvailable && irisAvailable) {
-    description = 'Log in with face or iris';
-  } else if (fingerprintAvailable && irisAvailable) {
-    description = 'Log in with fingerprint or iris';
-  } else if (facialRecognitionAvailable) {
-    description = 'Log in with face';
-  } else if (fingerprintAvailable) {
-    description = 'Log in with fingerprint ';
-  } else if (irisAvailable) {
-    description = 'Log in with iris';
-  } else {
-    description = 'No biometric authentication methods available';
-  }
-
-        
-  
-
-
 
     return(
         <View>
           <GovukButton>
                 <GovukButtonText
                     onPress={ () => {
-                      if ((facialRecognitionAvailable || fingerprintAvailable || irisAvailable) && result == null) {
-                        authenticate();
-                      } else {
-                        login();
-                      }
-                      
+                      login();
                     }
                     }
                     >
