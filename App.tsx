996fc08383a0d0debac1e71a03ad78db587395cd
Chris Hyland <95708546+chrishylanduk@users.noreply.github.com>
2023-06-19 19:39:13 +0000

    CH: Extract push notification register method

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ import { PageContext, UserIdContext } from "./app/views/Context";
 import { PageType } from "./app/views/types/ContextTypes";
 import SignInLoadingSection from "./app/views/signed_in_stack";
 import { PushNotificationTokenContext } from "./app/views/Context";
+import registerForPushNotificationsAsync from "./app/utilities/push_notifications/Register";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -25,42 +26,6 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
-
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Failed to get push token for push notification!");
-      return;
-    }
-    try {
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-    } catch (error) {
-      alert(error);
-    }
-    console.log(token);
-  } else {
-    alert("Must use physical device for Push Notifications");
-  }
-
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-
-  return token;
-}
 
 function App() {
   const [expoPushToken, setExpoPushToken] = React.useState<String>("");
