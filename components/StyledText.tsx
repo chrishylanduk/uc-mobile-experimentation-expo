ad75a423460504ed03cca3a37fce24712bb4d277
aidan23smith <aidan23smith@gmail.com>
2023-06-23 13:11:37 +0100

    AS: Delete unused files

--- components/StyledText.tsx
+++
@@ -1,6 +0,0 @@
-import { Text, type TextProps } from "./Themed";
-import React, { type ReactElement } from "react";
-
-export function MonoText(props: TextProps): ReactElement {
-  return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
-}
