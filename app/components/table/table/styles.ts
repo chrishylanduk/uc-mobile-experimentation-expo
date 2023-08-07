05b1a11505283ba14bc000adbfa2ad739c263ce0
Chris Hyland <95708546+chrishylanduk@users.noreply.github.com>
2023-08-07 15:50:45 +0100

    @MOBEXP-56 - Address review comments

--- app/components/table/table/styles.ts
+++ app/components/table/table/styles.ts
@@ -1,36 +1,7 @@
 import { StyleSheet } from "react-native";
+import { GOVUK_BORDER_COLOUR } from "../../constants/colours";
 
 export const govukTableStyle = StyleSheet.create({
   border: {},
-  style: { borderBottomWidth: 1, borderBottomColor: "#b1b4b6" },
-  // table: {
-  //   fontFamily: "arial",
-  //   fontWeight: "400",
-  //   fontSize: 16,
-  //   lineHeight: 1.25,
-  //   color: "#0b0c0c",
-  //   width: "100%",
-  //   marginBottom: 20,
-  //   borderSpacing: 0,
-  //   borderCollapse: "collapse",
-  // },
-  // header: {
-  //   fontWeight: "700",
-  //   paddingTop: 10,
-  //   paddingRight: 20,
-  //   paddingBottom: 10,
-  //   paddingLeft: 0,
-  //   borderBottom: "1px solid #b1b4b6",
-  //   textAlign: "left",
-  //   verticalAlign: "top",
-  // },
-  // cell: {
-  //   paddingTop: 10,
-  //   paddingRight: 20,
-  //   paddingBottom: 10,
-  //   paddingLeft: 0,
-  //   borderBottom: "1px solid #b1b4b6",
-  //   textAlign: "left",
-  //   verticalAlign: "top",
-  // },
+  style: { borderBottomWidth: 1, borderBottomColor: GOVUK_BORDER_COLOUR },
 });
